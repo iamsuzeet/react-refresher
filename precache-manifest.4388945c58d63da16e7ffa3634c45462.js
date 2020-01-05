6803e048899eb4e87002a26e1d27a8fcbe94e181
iamsuzeet <iamsuzeet@gmail.com>
2020-01-05 13:44:55 +0545

    Updates

---
+++ precache-manifest.4388945c58d63da16e7ffa3634c45462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e879f1fdea1c88439baec5c23f44ff8e",
+    "url": "/react-refresher/index.html"
+  },
+  {
+    "revision": "2faa8736015e8c3c9e51",
+    "url": "/react-refresher/static/css/main.6551ae92.chunk.css"
+  },
+  {
+    "revision": "1c16fbb776eb4292d8ea",
+    "url": "/react-refresher/static/js/2.edbf9c7e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-refresher/static/js/2.edbf9c7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2faa8736015e8c3c9e51",
+    "url": "/react-refresher/static/js/main.bf5dd24a.chunk.js"
+  },
+  {
+    "revision": "c30168cbd0421d69fba8",
+    "url": "/react-refresher/static/js/runtime-main.1b7e02a9.js"
+  }
+]);
